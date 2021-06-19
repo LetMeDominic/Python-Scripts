4106f89940e84508a50864468ac9c1cb886796bf
Shark <85628816+RichBoyDom@users.noreply.github.com>
2021-06-19 04:04:34 -0500

    Create phrase.py

---
+++ phrase.py
@@ -0,0 +1,7 @@
+import random
+nouns = ("puppy", "car", "rabbit", "girl", "monkey")
+verbs = ("runs", "hits", "jumps", "drives", "barfs") 
+adv = ("crazily.", "dutifully.", "foolishly.", "merrily.", "occasionally.")
+adj = ("adorable", "clueless", "dirty", "odd", "stupid")
+num = random.randrange(0,5)
+print(nouns[num],verbs[num],adv[num],adj[num])
